5d007118f24582781c7ba6027e92628a232dbb83
Jacob <jacobulysseslockwood@gmail.com>
2025-03-22 17:55:11 -0400

    add head and foot

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
 import { createSignal } from "solid-js";
 
-function App() {
-  const url = new URL(location.href);
-  const initialCode = decodeURIComponent(url.searchParams.get("r") ?? "");
-  const [code, setCode] = createSignal(initialCode);
-  const updCode = (s: string) => {
-    url.searchParams.set("r", encodeURIComponent(s));
-    history.pushState({}, "", url.toString());
-    setCode(s);
+function createSearchSignal(key: string, def: string = "") {
+  const init = decodeURIComponent(
+    new URL(location.href).searchParams.get(key) ?? def
+  );
+  const [val, setVal] = createSignal(init);
+  const set = (newval: string) => {
+    const u = new URL(location.href);
+    u.searchParams.set(key, encodeURIComponent(newval));
+    history.pushState({}, "", u.toString());
+    setVal(newval);
   };
+  return { init, val, set };
+}
+function inp(param: ReturnType<typeof createSearchSignal>) {
+  return (ev: InputEvent & { target: HTMLTextAreaElement }) =>
+    param.set(ev.target.value);
+}
+
+function App() {
+  const head = createSearchSignal("h");
+  const code = createSearchSignal("c");
+  const foot = createSearchSignal("f");
   const byt = new TextEncoder();
   const chr = new Intl.Segmenter("en", { granularity: "grapheme" });
-  const charcount = () => [...chr.segment(code())].length;
-  const bytecount = () => byt.encode(code()).length;
+  const charcount = () => [...chr.segment(code.val())].length;
+  const bytecount = () => byt.encode(code.val()).length;
   return (
     <div class="font-mono">
       <div class="flex flex-col">
         <label for="head">Header:</label>
-        <textarea name="head" id="head"></textarea>
+        <textarea name="head" id="head" onInput={inp(head)}>
+          {head.init}
+        </textarea>
         <label for="code">
           Code: ({charcount()} characters, {bytecount()} bytes)
         </label>
-        <textarea
-          name="code"
-          id="code"
-          onInput={(ev) => updCode(ev.target.value)}
-        >
-          {initialCode}
+        <textarea name="code" id="code" onInput={inp(code)}>
+          {code.init}
         </textarea>
         <label for="foot">Footer:</label>
-        <textarea name="foot" id="foot"></textarea>
+        <textarea name="foot" id="foot" onInput={inp(foot)}>
+          {foot.init}
+        </textarea>
       </div>
     </div>
   );
